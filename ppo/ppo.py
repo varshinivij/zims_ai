18b490832e54e1561044364529e25112a0a0e613
Varshini Vijay <varshinivijay05@gmail.com>
2026-02-24 05:53:57 -0800

    comments

--- ppo/ppo.py
+++ ppo/ppo.py
@@ -6,6 +6,9 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.distributions.categorical import Categorical
 
+"""
+Memory function for PPO that stores 1) state 2) reward 3) action 4) log probability of action as per policy 5) value as per critic
+"""
 class PPOMemory:
     def __init__(self, batch_size):
         self.rewards = []
@@ -18,7 +21,7 @@ class PPOMemory:
 
     def generate_batches(self):
         n_states = len(self.states)
-        batch_start = np.arange(0, n_states, self.batch_size)
+        batch_start = np.arange(0,  n_states, self.batch_size)
         indices = np.arange(n_states, dtype=np.int64)
         np.random.shuffle(indices)
         batches = [indices[i:i + self.batch_size] for i in batch_start]
@@ -39,7 +42,10 @@ class PPOMemory:
         self.probs.append(prob)
         self.dones.append(done)
 
-    
+"""
+Actor Network with 2 hidden layers with 256 neurons, relu activation function, softmax output activation function. 
+Adam optimizer (for gradient descent)
+"""
 class ActorNetwork(nn.Module):
     def __init__(self, n_actions, input_dims, alpha,
             fc1_dims=256, fc2_dims=256, chkpt_dir='tmp/ppo'):
@@ -116,6 +122,7 @@ class Agent:
     def save_memory(self, state, action, probs, vals, reward, done):
         self.memory.store_memory(state, action, probs, vals, reward, done)
 
+    #interface functionality 
     def save_models(self):
         print('... saving models ...')
         self.actor.save_checkpoint()
@@ -125,3 +132,68 @@ class Agent:
         print('... loading models ...')
         self.actor.load_checkpoint()
         self.critic.load_checkpoint()
+
+    #given state, find the action, probability of action and corresponding value
+    def choose_action(self, observation):
+        state = T.tensor([observation], dtype=T.float).to(self.actor.device)
+
+        dist = self.actor(state)
+        value = self.critic(state)
+        action = dist.sample()
+
+        probs = T.squeeze(dist.log_prob(action)).item()
+        action = T.squeeze(action).item()
+        value = T.squeeze(value).item()
+
+        return action, probs, value
+
+    def learn(self):
+        for _ in range(self.n_epochs):
+            state_arr, action_arr, old_prob_arr, vals_arr,\
+            reward_arr, dones_arr, batches = \
+                    self.memory.generate_batches()
+
+            values = vals_arr
+            advantage = np.zeros(len(reward_arr), dtype=np.float32)
+
+            for t in range(len(reward_arr)-1):
+                discount = 1
+                a_t = 0
+                for k in range(t, len(reward_arr)-1):
+                    a_t += discount*(reward_arr[k] + self.gamma*values[k+1]*\
+                            (1-int(dones_arr[k])) - values[k])
+                    discount *= self.gamma*self.gae_lambda
+                advantage[t] = a_t
+            advantage = T.tensor(advantage).to(self.actor.device)
+
+            values = T.tensor(values).to(self.actor.device)
+            for batch in batches:
+                states = T.tensor(state_arr[batch], dtype=T.float).to(self.actor.device)
+                old_probs = T.tensor(old_prob_arr[batch]).to(self.actor.device)
+                actions = T.tensor(action_arr[batch]).to(self.actor.device)
+
+                dist = self.actor(states)
+                critic_value = self.critic(states)
+
+                critic_value = T.squeeze(critic_value)
+
+                new_probs = dist.log_prob(actions)
+                prob_ratio = new_probs.exp() / old_probs.exp()
+                #prob_ratio = (new_probs - old_probs).exp()
+                weighted_probs = advantage[batch] * prob_ratio
+                weighted_clipped_probs = T.clamp(prob_ratio, 1-self.policy_clip,
+                        1+self.policy_clip)*advantage[batch]
+                actor_loss = -T.min(weighted_probs, weighted_clipped_probs).mean()
+
+                returns = advantage[batch] + values[batch]
+                critic_loss = (returns-critic_value)**2
+                critic_loss = critic_loss.mean()
+
+                total_loss = actor_loss + 0.5*critic_loss
+                self.actor.optimizer.zero_grad()
+                self.critic.optimizer.zero_grad()
+                total_loss.backward()
+                self.actor.optimizer.step()
+                self.critic.optimizer.step()
+
+        self.memory.clear_memory()               
